dbce682cb0867830e48ce5e76672a2546760f6c7
hhimanshu <harit.himanshu@yahoo.in>
2021-06-21 07:31:14 -0700

    feat(DEV-11): add comment for readers to know where to make change

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import '@fontsource/poppins/900.css';
 import App from './components/App';
 import { Content } from './components/Content';
 
+// This is where your application content can be mounted
 const Root = () => <App children={<Content />} />;
 
 ReactDOM.render(<Root />, document.getElementById('root'));
