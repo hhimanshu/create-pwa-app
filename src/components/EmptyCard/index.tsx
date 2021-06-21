fb54efd020177a4c53aa595510ec33369268ab31
hhimanshu <harit.himanshu@yahoo.in>
2021-06-21 08:18:55 -0700

    style(DEV-12): format styles

--- src/components/EmptyCard/index.tsx
+++ src/components/EmptyCard/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 import { Card } from '../Card';
-import styles from './emptyCard.module.css'
-import Offline from '../../icons/offline.svg'
+import styles from './emptyCard.module.css';
+import Offline from '../../icons/offline.svg';
 export const EmptyCard = () => (
   <Card>
     <div className={styles.emptyCardContainer}>
-      <Offline/>
+      <Offline />
       <p>You are currently offline</p>
     </div>
   </Card>
-)
+);
