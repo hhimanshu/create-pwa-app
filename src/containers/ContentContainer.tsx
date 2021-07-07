5e8f3146218d9e1d2e81d1745dc68aaf6b1f5fd4
hhimanshu <harit.himanshu@yahoo.in>
2021-07-07 13:49:22 -0700

    feat(DEV-13): apply StateWhileRevalidate strategy for caching responses

--- src/containers/ContentContainer.tsx
+++ src/containers/ContentContainer.tsx
@@ -3,9 +3,11 @@ import { Content } from '../components/Content';
 import AppShell from '../components/AppShell';
 
 export const ContentContainer = () => {
+
   return (
     <AppShell>
       <Content />
+      {!window.navigator.onLine && <p>You are offline</p>}
     </AppShell>
   );
 };
